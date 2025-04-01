2a12dd79453126bb380f103847af5f3a05665e81
ellagie <neilgieendab@gmail.com>
2025-04-01 17:12:22 +0800

    script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", function () {
   AOS.init({
-    duration: 1000,
-    easing: "ease-in-out",
-    once: true,
+    duration: 1000,     // Controls the duration of the animation (in milliseconds)
+    easing: "ease-in-out", // Specifies the easing function for animation
+    once: true,        // Ensures the animation happens only once (on scroll)
   });
 });
