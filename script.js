ee26fa521db39953c355c7d6fcaf5a51f59f2ae1
ellagie <neilgieendab@gmail.com>
2025-04-08 00:27:52 +0800

    script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-// script.js
-document.addEventListener("DOMContentLoaded", function() {
-  AOS.init();
-  fetch('content.html')
-    .then(response => response.text())
-    .then(data => document.getElementById('content').innerHTML = data);
-});
