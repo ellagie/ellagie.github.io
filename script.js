31c63f0f887007b223c1d9127ee88714b6f67a04
ellagie <neilgieendab@gmail.com>
2025-07-04 19:57:53 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,1 @@
-// Selecting necessary elements
-const hamburgerMenu = document.querySelector('.hamburger-menu');
-const navLinks = document.querySelector('.nav-links');
 
-// Function to toggle the menu
-function toggleMenu() {
-    // Toggle the 'active' class on the hamburger menu
-    hamburgerMenu.classList.toggle('active');
-
-    // Toggle the visibility of the navigation links
-    if (navLinks.style.display === 'block') {
-        navLinks.style.display = 'none';
-    } else {
-        navLinks.style.display = 'block';
-    }
-}
-
-// Event listener for hamburger menu click
-hamburgerMenu.addEventListener('click', toggleMenu);
